0256f307664658db6a57c98540c729b7bd6c3d26
antkera <128487289+antkera@users.noreply.github.com>
2023-11-01 17:49:04 +0100

    principio completado

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
-
+hbs.registerPartials('views/partials');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
@@ -19,7 +19,28 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index.hbs');
+});
+
+app.get(`/beers`, (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(response => {
+      res.render(`beers.hbs`, { listBeers: response });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+app.get(`/random-beer`, (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(response => {
+      res.render(`random-beer.hbs`, response[0]);
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
